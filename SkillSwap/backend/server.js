7ea23ef64b9d55504c1c8a8b122d5774e14ee7e5
Anshika22102 <guptaanshika2212@gmail.com>
2024-12-06 11:14:58 +0530

    changes

--- SkillSwap/backend/server.js
+++ SkillSwap/backend/server.js
@@ -8,12 +8,14 @@ const cors = require('cors');
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = 'mongodb://localhost:27017/SkillSwap';
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Database connection
+
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected!'))
